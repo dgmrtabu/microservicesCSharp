cc4db6ca1f08c13b224bd8782015fa9eea523092
DAVID GABRIEL MONTAÑO ROCHA <david.montano@cns.gob.bo>
2021-09-30 10:40:24 -0400

    Agregar archivos de proyecto.

---
+++ TiendaServicios.Api.Autor/Modelo/GradoAcademico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaServicios.Api.Autor.Modelo
+{
+    public class GradoAcademico
+    {
+
+        public int GradoAcademicoId { get; set; }
+        public int Nombre { get; set; }
+        public int CentroAcademico { get; set; }
+        public DateTime? FechaGrado { get; set; }
+        public int AutorLibroId { get; set; }
+        public AutorLibro AutorLibro { get; set; }
+
+        public string GradoAcademicoGuid { get; set; }
+
+    }
+}
