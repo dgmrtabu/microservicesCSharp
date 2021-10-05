fa1b74e4faa1ad161afcdc844056b53e3addff5c
DAVID GABRIEL MONTAÑO ROCHA <david.montano@cns.gob.bo>
2021-10-05 09:27:36 -0400

    Entity Frameword SQL-Server

---
+++ TiendaServicios.Api.Libro/Persistencia/ContextoLibreria.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Libro.Modelo;
+
+namespace TiendaServicios.Api.Libro.Persistencia
+{
+    public class ContextoLibreria : DbContext
+
+    {
+        public ContextoLibreria(DbContextOptions<ContextoLibreria> options) : base(options) { } //Setear la cadena de conexcion desde la clase Startup
+
+        public DbSet<LibreriaMaterial> LibreriaMaterial { get; set; }
+    }
+}
