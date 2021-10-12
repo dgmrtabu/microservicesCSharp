1d4146f649436d5e854153dd126e77fd28e726ee
DAVID GABRIEL MONTAÑO ROCHA <david.montano@cns.gob.bo>
2021-10-12 16:29:26 -0400

    Crear Clase Consulta Registros

--- TiendaServicios.Api.Autor/Modelo/AutorLibro.cs
+++ TiendaServicios.Api.Autor/Modelo/AutorLibro.cs
@@ -11,7 +11,6 @@ namespace TiendaServicios.Api.Autor.Modelo
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public DateTime? FechaNacimiento { get; set; }
-
         public ICollection<GradoAcademico> ListaGradoAcademico { get; set; }
         public string AutorLibroGuid { get; set; }
     }
